eae602e29f8aff78c0a288586be9f9319420ee96
JustinaLam <88551260+JustinaLam@users.noreply.github.com>
2022-09-09 23:16:46 -0400

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -138,4 +138,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 django_heroku.settings(locals(),staticfiles=False)
