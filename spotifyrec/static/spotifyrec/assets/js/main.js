b05a982ad050c308d1cff451e94956e8667aa942
JustinaLam <88551260+JustinaLam@users.noreply.github.com>
2022-09-09 23:26:08 -0400

    Add files via upload

---
+++ spotifyrec/static/spotifyrec/assets/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+
+    //End
+});
